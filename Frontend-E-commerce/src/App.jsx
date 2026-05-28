70e4f8602fc8951f5b49f13998041500cbe8600c
pheakdeybombastic <microstore@MacBook-Pro.local>
2026-05-28 15:17:41 +0700

    change  the words github to pornhub

--- Frontend-E-commerce/src/App.jsx
+++ Frontend-E-commerce/src/App.jsx
@@ -70,7 +70,7 @@ function App() {
                 >
                   <use href="/icons.svg#github-icon"></use>
                 </svg>
-                GitHub
+                pornhub
               </a>
             </li>
             <li>
