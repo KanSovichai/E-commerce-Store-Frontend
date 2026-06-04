7b1f65f0eb1d3c4a2e266b7f781f82186da9747f
Kan Sovichai <ksovichai08@gmail.com>
2026-06-04 16:53:12 +0700

    Created broken navbar

---
+++ Frontend-E-commerce/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import "/src/styles/Navbar.css"
+const Nav = ()=> {
+    return(
+        <div>
+            <div className="navbar_container">
+                <div className="logo">
+                    <img src="../src/assets/OnlyTech.png" alt="" />
+                </div>
+                <div className="brokenNavImg">
+                    <img src="../src/assets/brokenParts.png" alt="" />
+                </div>
+                <div className="navigations">
+
+                </div>
+            </div>
+        </div>
+    );
+};  
+export default Nav;
